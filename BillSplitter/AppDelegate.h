09711a47600ac402e628a627340baecc4de9b1a0
Varninder Rai <varninder@openmailbox.org>
2017-08-05 16:08:17 -0700

    Initial Commit

---
+++ BillSplitter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BillSplitter
+//
+//  Created by Livleen Rai on 2017-08-05.
+//  Copyright © 2017 Livleen Rai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BillSplitterTests/BillSplitterTests.m
@@ -0,0 +1,39 @@
+//
+//  BillSplitterTests.m
+//  BillSplitterTests
+//
+//  Created by Livleen Rai on 2017-08-05.
+//  Copyright © 2017 Livleen Rai. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface BillSplitterTests : XCTestCase
+
+@end
+
+@implementation BillSplitterTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
