4cd3d9e23c85e93c47254fc5188f0f2883a67f73
Varninder Rai <varninder@openmailbox.org>
2017-08-06 23:23:35 -0700

    Add calculate function

--- BillSplitter/ViewController.m
+++ BillSplitter/ViewController.m
@@ -9,6 +9,9 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *billValueTextField;
+@property (weak, nonatomic) IBOutlet UISlider *numberOfPeopleSlider;
+@property (weak, nonatomic) IBOutlet UILabel *billSplitLabel;
 
 @end
 
@@ -16,6 +19,8 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    
     // Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -23,6 +28,17 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+- (IBAction)calculateSplitAmount:(UIButton *)sender {
+    
+    NSDecimalNumber *billAmount = [NSDecimalNumber decimalNumberWithString:_billValueTextField.text];
+    NSDecimalNumber *sliderValue = [[NSDecimalNumber alloc]initWithDouble:_numberOfPeopleSlider.value];
+    
+    NSDecimalNumber *billSplit = [billAmount decimalNumberByDividingBy:sliderValue];
+    
+    _billSplitLabel.text = [NSString stringWithFormat:@"You should each cough up $%0.2f", [billSplit floatValue]];
+    
+    
 }
 
 
